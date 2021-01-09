264a0b5cac7215a89c79554f4f390482b830093d
yuriDuque <yurithielmann83@gmail.com>
2021-01-08 21:03:39 -0300

    estilizando formulário de cadastro

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,19 +1,24 @@
 import React from "react";
-import { Button, TextField } from "@material-ui/core";
+import { Button, TextField, Switch, FormControlLabel } from "@material-ui/core";
 
 function FormularioCadastro() {
   return (
     <form>
-      <TextField id="nome" label="Nome" variant="outlined" fullWidth/>
+      <TextField id="nome" label="Nome" variant="outlined" margin="normal" fullWidth />
 
-      <TextField id="sobrenome" label="Sobrenome" variant="outlined" fullWidth/>
+      <TextField id="sobrenome" label="Sobrenome" variant="outlined" margin="normal" fullWidth />
 
-      <TextField id="cpf" label="CPF" variant="outlined" fullWidth/>
+      <TextField id="cpf" label="CPF" variant="outlined" margin="normal" fullWidth />
 
-      <label>Promoções</label>
-      <input type="checkbox" />
-      <label>Novidades</label>
-      <input type="checkbox" />
+      <FormControlLabel
+        label="Promoções"
+        control={<Switch name="promocoes" defaultChecked={true} color="primary" />}
+      />
+
+      <FormControlLabel
+        label="Novidades"
+        control={<Switch name="novidades" defaultChecked={true} color="primary" />}
+      />      
 
       <Button type="submit" variant="contained" color="primary">
         Cadastrar
