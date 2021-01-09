4cf9bceec73668efc39acb2cdca890859ba339df
yuriDuque <yurithielmann83@gmail.com>
2021-01-08 21:27:06 -0300

    utilizando react hooks

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -3,16 +3,16 @@ import { Button, TextField, Switch, FormControlLabel } from "@material-ui/core";
 
 function FormularioCadastro() {
   const [nome, setNome] = useState("");
-  let sobrenome = "";
-  let cpf = "";
+  const [sobrenome, setSobrenome] = useState("");
+  const [cpf, setCpf] = useState("");
 
   return (
     <form onSubmit={event => {event.preventDefault()}}>
       <TextField value={nome} onChange={event => setNome(event.target.value)} id="nome" label="Nome" variant="outlined" margin="normal" fullWidth />
 
-      <TextField id="sobrenome" label="Sobrenome" variant="outlined" margin="normal" fullWidth />
+      <TextField value={sobrenome} onChange={event => setSobrenome(event.target.value)} id="sobrenome" label="Sobrenome" variant="outlined" margin="normal" fullWidth />
 
-      <TextField id="cpf" label="CPF" variant="outlined" margin="normal" fullWidth />
+      <TextField value={cpf} onChange={event => setCpf(event.target.value)} id="cpf" label="CPF" variant="outlined" margin="normal" fullWidth />
  
       <FormControlLabel
         label="Promoções"
